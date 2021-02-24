269387373000ef5c1037fc2a94c43cf4a8dc1cae
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-24 14:45:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let first = 0
+let second = 0
+let total = 0
+document.getElementById('press').addEventListener('click', myInput)
+function myInput () {
+  first = document.getElementById('enter-one').value
+  first = parseInt(first)
+  second = document.getElementById('enter-two').value
+  second = parseInt(second)
+  for (first = first; first > 0; first--) {
+  total = total + second
+  }
+  alert(total)
+}
